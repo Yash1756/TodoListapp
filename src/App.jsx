3bf6ac33d10159e6ecde5c2defe49ccb293eb047
Yash1756 <143863549+Yash1756@users.noreply.github.com>
2023-09-25 20:24:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import './App.css'
+import React, { useState } from 'react'
+
+const App = () => {
+   const [title, settitle] = useState("")
+   const [desc, setdesc] = useState("")
+   const [maintask, setmaintask] = useState([])
+   
+   
+  const submitHandler= (e)=>{
+      e.preventDefault()
+     setmaintask([...maintask,{title,desc}])
+      settitle("")
+      setdesc("")
+  }
+  let rendertask = <h2>No Task Avialible</h2>
+  
+
+  if(maintask.length>0){
+    rendertask = maintask.map((t,i)=>{
+      return <div key={i} className="showtask">
+        <h2>{t.title}</h2>
+        <h3>{t.desc}</h3>
+        <button
+        onClick={()=>{
+          deleteHandler (i);
+        }}
+        >Delete</button>
+      </div>
+      
+    })
+  }
+ 
+  const deleteHandler = (i) =>{
+        let copytask = [...maintask]
+        copytask.splice(i,1)
+        setmaintask(copytask)
+  }
+
+  return (
+    <>
+      <div className="header"><h1>MY <span>TO DO List</span></h1></div>
+       
+       <form onSubmit={submitHandler}> 
+       
+       <input type="text" className='title' placeholder='Enter Title Here'
+       value={title} 
+       onChange={(e)=>{
+        settitle(e.target.value)
+       }}/>
+       <input type="text" className='desc' placeholder='Enter Description Here'
+       value={desc}
+       onChange={(e)=>{
+        setdesc(e.target.value)
+       }} />
+       <button>Add Task</button>
+     </form>
+   
+     
+     <div className="task">
+      {rendertask}
+     </div>
+      
+    </>
+  )
+}
+
+export default App
+
